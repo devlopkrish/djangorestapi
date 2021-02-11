5654b0c8de9c34088522fb388de93e35319b3451
devlopkrish <54771433+devlopkrish@users.noreply.github.com>
2021-02-11 23:48:07 +0530

    Update settings.py

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2psyy0g^5@h&@41p4+1-$&7+-*sot-%p#=i5oiq4qeo7e0rq!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
